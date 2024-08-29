e02bda38457f54deaa4fc0e90b228ce07b18d654
samuelfrnk <samuel.frank283@gmail.com>
2024-08-29 17:40:57 +0200

    path loss modell comparisation

---
+++ Experiments/Evaluation/CSV_Path_Loss_Comparer.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import numpy as np
+
+data = pd.read_csv('Aggregated_Ex_1_and_2.csv')
+
+rssi_ref = -66.79
+n = 2
+
+
+def estimate_distance(rssi, rssi_ref, n):
+    return 10 ** ((rssi_ref - rssi) / (10 * n))
+
+
+data['estimated_distance'] = estimate_distance(data['average_rssi'], rssi_ref, n)
+
+# Convert Distance and estimated_distance to numeric types
+data['Distance'] = pd.to_numeric(data['Distance'], errors='coerce')
+data['estimated_distance'] = pd.to_numeric(data['estimated_distance'], errors='coerce')
+
+# Calculate the error
+data['error'] = np.abs(data['Distance'] - data['estimated_distance'])
+
+result = data[['Distance', 'average_rssi', 'estimated_distance', 'error']]
+
+result.to_csv('estimated_distances.csv', index=False)
+
