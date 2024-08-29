b6a658199c2d192c2e4112c136aa53820d294fee
samuelfrnk <samuel.frank283@gmail.com>
2024-08-29 10:02:08 +0200

    rename file

--- Experiments/Evaluation/Data_Evaluation.py
+++ Experiments/Evaluation/Data_Evaluation.py
@@ -1,1 +1,2 @@
-#This program is dedicated to aggregate the data from the experiments into a CSV file and give additional data about the measurements in a md file 
+#This program is dedicated to aggregate the data from the experiments into a CSV file. 
+ 
