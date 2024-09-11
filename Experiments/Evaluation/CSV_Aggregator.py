b9c5bdd721912505c7b10ace3ea51f800d41d507
samuelfrnk <samuel.frank283@gmail.com>
2024-09-11 12:02:55 +0200

    adjusted Evaluation scripts and experiments with replaced batteries

--- Experiments/Evaluation/CSV_Aggregator.py
+++ Experiments/Evaluation/CSV_Aggregator.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-file_name = 'YOUR_FILENAME.csv'
+file_name = 'Experiment_with_No_Batteries.csv'
 df = pd.read_csv(file_name)
 
 df['Timestamp'] = pd.to_datetime(df['Timestamp'], errors='coerce')
--- Experiments/Evaluation/CSV_Converter.py
+++ Experiments/Evaluation/CSV_Converter.py
@@ -4,11 +4,11 @@ from datetime import datetime, timedelta
 
 # This program is dedicated to aggregate the data from the experiments into a CSV file.
 
-filename = 'EXPERIMENT_LOGS.rtf'
-distances = ['distanceAtIdx1', 'distanceAtIdx2', '...']
+filename = 'Batteries_replaced_11_21.rtf'
+distances = [0]
 
 # The exact time when you started the experiment by enabling logs in nRF board for the first time
-time_of_start_experiment = datetime(2024, 8, 28, 14, 41, 0)
+time_of_start_experiment = datetime(2024, 9, 11, 11, 21, 0)
 
 
 # This global variable keeps track of the distance of the individual measurements. It will be changed during the
@@ -26,14 +26,14 @@ with (open(filename.split('.')[0] + '.csv', 'w') as csvfile):
     writer.writeheader()
 
     for i in range(0, len(lines)):
-        if lines[i].startswith(' app: Current index: '):
-            indexCurrent = lines[i].split(' app: Current index: ')[1][:-2]
+        if lines[i].startswith('<info> app: Current index: '):
+            indexCurrent = lines[i].split('<info> app: Current index: ')[1][:-2]
             indexCurrentAsInt = int(indexCurrent)
 
-        if lines[i].startswith(' app: AirTag Identified'):
-            mac_address = lines[i+1].split(' app: Peer Address is ')[1][:-2]
-            rssi_value = int(lines[i+2].split(',')[0].split(' app: RSSI: ')[1])
-            time_since_start = int(lines[i+2].split(',')[1].split(' timestamp: ')[1][:-2])
+        if lines[i].startswith('<info> app: AirTag Identified'):
+            mac_address = lines[i+1].split('<info> app: Peer Address is ')[1][:-2]
+            rssi_value = int(lines[i+2].split(',')[0].split('<info> app: RSSI: ')[1])
+            time_since_start = int(lines[i+2].split(',')[1].split('timestamp: ')[1][:-2])
             timestamp_of_measurement = time_of_start_experiment + timedelta(seconds=time_since_start)
             time = timestamp_of_measurement.strftime('%Y-%m-%d %H:%M:%S')
             distance = distances[indexCurrentAsInt]
